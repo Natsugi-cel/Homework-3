fdeed9a711d3132c7a62429ce6c6c6b414ca5048
kolom <s_kolomeetzc@ukr.net>
2024-01-11 20:47:17 +0100

    Homework second finish

--- main.py
+++ main.py
@@ -1,45 +1,55 @@
 # print("Hello world")
 
-try:
-    print("1. Monday\n2. Tuesday\n3. Wednesday\n4. Thursday\n5. Friday\n6. Saturday\n7. Sunday ")
-    user_select = int(input("Enter menu number: "))
+# try:
+#     print("1. Monday\n2. Tuesday\n3. Wednesday\n4. Thursday\n5. Friday\n6. Saturday\n7. Sunday ")
+#     user_select = int(input("Enter menu number: "))
+# #
+#     # v1
+#     if user_select == 1:
+#         print("Monday")
+#     elif user_select == 2:
+#         print("Tuesday")
+#     elif user_select == 3:
+#         print("Wednesday")
+#     elif user_select == 4:
+#         print("Thursday")
+#     elif user_select == 5:
+#         print("Friday")
+#     elif user_select == 6:
+#         print("Suturday")
+#     elif user_select == 7:
+#         print("Sunday")
+#     else:
+#         print("Incorrect menu item!")
 #
-    # v1
-    if user_select == 1:
-        print("Monday")
-    elif user_select == 2:
-        print("Tuesday")
-    elif user_select == 3:
-        print("Wednesday")
-    elif user_select == 4:
-        print("Thursday")
-    elif user_select == 5:
-        print("Friday")
-    elif user_select == 6:
-        print("Suturday")
-    elif user_select == 7:
-        print("Sunday")
-    else:
-        print("Incorrect menu item!")
+#     # match user_select:
+#     #         case == 1:
+#     #            print("Monday")
+#     #         case == 2:
+#     #            print("Tuesday")
+#     #         case == 3:
+#     #            print("Wednesday")
+#     #         case == 4:
+#     #            print("Thursday")
+#     #         case == 5:
+#     #            print("Friday")
+#     #         case == 6:
+#     #            print("Saturday")
+#     #         case == 7:
+#     #            print("Sunday")
+#     #         case_:
+#     #             print("incorrect menu item!")
+#
+# except Exception as e:
+#     print(f"Error: {e}")
 
-    # match user_select:
-    #         case == 1:
-    #            print("Monday")
-    #         case == 2:
-    #            print("Tuesday")
-    #         case == 3:
-    #            print("Wednesday")
-    #         case == 4:
-    #            print("Thursday")
-    #         case == 5:
-    #            print("Friday")
-    #         case == 6:
-    #            print("Saturday")
-    #         case == 7:
-    #            print("Sunday")
-    #         case_:
-    #             print("incorrect menu item!")
+####################### 2 ############
 
-except Exception as e:
-    print(f"Error: {e}")
-
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+if num1 != num2 and num1 < num2:
+   print(str(num1) + " " + str(num2))
+elif num1 != num2 and num1 > num2:
+   print(str(num2) + " " + str(num1))
+else:
+   print("num1 = num2")
